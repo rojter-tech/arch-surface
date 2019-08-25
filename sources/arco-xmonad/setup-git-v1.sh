3e1d5237163601dc7cdbe65d0012af8ca83310c7
Daniel Reuter <dreuter@kth.se>
2019-08-25 22:12:26 +0200

    change

--- sources/arco-xmonad/setup-git-v1.sh
+++ sources/arco-xmonad/setup-git-v1.sh
@@ -17,9 +17,9 @@ set -e
 # change into your name and email.
 
 git init
-git config --global user.name "Erik Dubois"
-git config --global user.email "erik.dubois@gmail.com"
-sudo git config --system core.editor nano
+git config --global user.name "Daniel Reuter"
+git config --global user.email "dreuter@kth.se"
+sudo git config --system core.editor vim
 git config --global credential.helper cache
 git config --global credential.helper 'cache --timeout=25000'
 git config --global push.default simple
