7bd1d52c6fa56b5140c183b29f81fc7cb2c8b521
Daniel Reuter <dreuter@kth.se>
2019-12-06 20:13:01 +0100

    Update 02-base-setup.sh

--- 02-base-setup.sh
+++ 02-base-setup.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
+groupadd -r autologin
+groupadd -r bumblebee
+groupadd -r docker
+useradd -m -g users \
+  -G wheel,storage,power,docker,autologin,audio,bumblebee \
+  -s /bin/bash dreuter
+passwd dreuter
+usermod -a -G wheel,storage,power,docker,autologin,audio,bumblebee dreuter
+vim /etc/sudoers
+#change %wheel ALL=(ALL) NOPASSWD: ALL
+su dreuter
+
 sudo pacman -Syyuu --noconfirm --needed
 sudo pacman -R vim
 sudo pacman -S p7zip unzip docker gvim \
   git rsync bash-completion wget which \
   reflector --noconfirm --needed
 sudo reflector --verbose --latest 200 --number 10 --sort rate --save /etc/pacman.d/mirrorlist
-mv /usr/bin/vi /usr/bin/vi.bak
-cp /usr/bin/vim /usr/bin/vi
-systemctl enable docker
-groupadd -r autologin
-groupadd -r bumblebee
-useradd -m -g users -G wheel,storage,power,docker,autologin,audio,bumblebee \
--s /bin/bash dreuter
-passwd dreuter
-usermod -a -G wheel,storage,power,docker,autologin,audio,bumblebee dreuter
-vim /etc/sudoers
-#change %wheel ALL=(ALL) NOPASSWD: ALL
-su dreuter
+sudo mv /usr/bin/vi /usr/bin/vi.bak
+sudo ln -s /usr/bin/vim /usr/bin/vi
+sudo systemctl enable docker
