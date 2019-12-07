d2cce0bf56c6d05704f9c680a5068395a073014f
Daniel Reuter <dreuter@kth.se>
2019-12-08 00:35:16 +0100

    Update 03-basic-software.sh

--- 03-basic-software.sh
+++ 03-basic-software.sh
@@ -40,6 +40,8 @@ sudo pacman -S gnome gnome-extra gnome-shell gdm --noconfirm --needed
 sudo systemctl enable gdm.service
 yay -S gnome-shell-extensions gnome-shell-extension-dash-to-dock \
   --answerclean N --answeredit N --noconfirm --needed
+sudo pacman -R gnome-terminal --noconfirm
+yay -S gnome-terminal-transparency --answerclean N --answeredit N --noconfirm --needed
 
 # i3
 sudo pacman -S xorg-server xorg-apps xorg-twm \
@@ -48,6 +50,11 @@ xterm rxvt-unicode --noconfirm --needed
 
 # lightdm
 sudo pacman -S lightdm lightdm-gtk-greeter --noconfirm --needed
+grep 'autologin-user=\|autologin-session=\|greeter-session=' /etc/lightdm/lightdm.conf && \
+sudo sed -i 's/#autologin-user=/autologin-user=dreuter/g' /etc/lightdm/lightdm.conf && \
+sudo sed -i 's/#autologin-session=/autologin-session=i3/g' /etc/lightdm/lightdm.conf && \
+sudo sed -i 's/#greeter-session=example-gtk-gnome/greeter-session=lightdm-gtk-greeter/g' /etc/lightdm/lightdm.conf && \
+grep 'autologin-user=\|autologin-session=\|greeter-session=' /etc/lightdm/lightdm.conf
 
 # Utils
 
@@ -63,37 +70,39 @@ sudo pacman -S bluez bluez-utils bluez-libs \
 pulseaudio pulseaudio-bluetooth pulseaudio-equalizer \
 pulseaudio-alsa alsa-utils alsa-plugins alsa-lib pavucontrol \
 pulseaudio-bluetooth bluez blueberry --noconfirm --needed
-amixer -c 0 sset 'Auto-Mute Mode' Disabled
-sudo alsactl store
-
 sudo systemctl enable bluetooth.service
 
 ### Command line utilities
 sudo pacman -S rxvt-unicode ranger rofi conky dmenu  \
 urxvt-perls perl-anyevent-i3 perl-json-xs --noconfirm --needed
-
 yay -S browsh gksu inxi-git --answerclean N --answeredit N --noconfirm --needed
 sudo pacman -S atool highlight elinks mediainfo w3m \
 ffmpegthumbnailer mupdf --noconfirm --needed
-
-yay -S firefox-nightly firefox-developer-edition visual-studio-code-bin \
-python-ptvsd --answerclean N --answeredit N --noconfirm --needed
-sudo pacman -S gimp chromium firefox vlc --noconfirm --needed
-
 yay -S urxvt-font-size-git python-pdftotext --answerclean N --answeredit N --noconfirm --needed
 
-grep 'autologin-user=\|autologin-session=\|greeter-session=' /etc/lightdm/lightdm.conf && \
-sudo sed -i 's/#autologin-user=/autologin-user=dreuter/g' /etc/lightdm/lightdm.conf && \
-sudo sed -i 's/#autologin-session=/autologin-session=i3/g' /etc/lightdm/lightdm.conf && \
-sudo sed -i 's/#greeter-session=example-gtk-gnome/greeter-session=lightdm-gtk-greeter/g' /etc/lightdm/lightdm.conf && \
-grep 'autologin-user=\|autologin-session=\|greeter-session=' /etc/lightdm/lightdm.conf
+### Browsers and code
+sudo pacman -S firefox chromium \
+  firefox-developer-edition vlc gimp --noconfirm --needed
+yay -S visual-studio-code-bin firefox-nightly python-ptvsd \
+  --answerclean N --answeredit N --noconfirm --needed
 
 sudo pacman -Syyuu --noconfirm --needed && \
 yay -Syyuu --noconfirm --needed
 
-sudo vim /etc/modprobe.d/alsa-base.conf
-#options snd_mia index=0
-#options snd_hda_intel index=1
+### Fix WiFi
+sudo iw dev wlp1s0 set power_save off # Fix only for this session
+# Permanent fix
+echo "[connection]" | sudo tee -a /etc/NetworkManager/NetworkManager.conf
+echo "wifi.powersave = 2" | sudo tee -a /etc/NetworkManager/NetworkManager.conf
+echo "[device]" | sudo tee -a /etc/NetworkManager/NetworkManager.conf
+echo "wifi.scan-rand-mac-address=false" | sudo tee -a /etc/NetworkManager/NetworkManager.conf
+cat /etc/NetworkManager/NetworkManager.conf
 
+### Fix sound
+amixer -c 0 sset 'Auto-Mute Mode' Disabled
+sudo alsactl store
+echo "options snd_mia index=0" | sudo tee -a /etc/modprobe.d/alsa-base.conf
+echo "options snd_hda_intel index=1" | sudo tee -a /etc/modprobe.d/alsa-base.conf
+cat /etc/modprobe.d/alsa-base.conf
 
-#sudo telinit 6
+sudo telinit 6
