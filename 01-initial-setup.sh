2fb10b5aa499d797ad3552b3d5ce6561c0f2df6d
Daniel Reuter <dreuter@kth.se>
2019-12-07 22:47:09 +0100

    Update 01-initial-setup.sh

--- 01-initial-setup.sh
+++ 01-initial-setup.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-pacstrap /mnt base base-devel linux linux-firmware openssh \
-  dhcpcd iw wpa_supplicant networkmanager dialog terminus-font
+# Assumes complete partition system mounted and 
+# ready, including EFI parition mounted as /boot/efi
+pacstrap /mnt base base-devel linux linux-firmware openssh terminus-font \
+  dhcpcd iw wpa_supplicant networkmanager dialog grub os-prober efibootmgr
 genfstab -U /mnt >> /mnt/etc/fstab
+
 arch-chroot /mnt
+systemctl enable dhcpcd.service
+systemctl enable sshd.service
+mkdir /boot/grub
+os-prober
+grub-mkconfig -o /boot/grub/grub.cfg
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+
 ln -sf /usr/share/zoneinfo/Europe/Stockholm /etc/localtime 
 ls -la /etc/localtime
 hwclock —-systohc
@@ -14,11 +24,13 @@ localectl set-keymap --no-convert us
 echo "KEYMAP=us" > /etc/vconsole.conf
 echo "FONT=ter-m32n" >> /etc/vconsole.conf
 echo "rOjterArchSurface" > /etc/hostname
-echo "127.0.0.1	localhost" >> /etc/hosts
-echo "127.0.1.1	rojterarchsurface.local	rojterarchsurface" >> /etc/hosts
-systemctl enable sshd.service
+echo "127.0.0.1    localhost" >> /etc/hosts
+echo "127.0.1.1    rojterarchsurface.lo    rojterarchsurface" >> /etc/hosts
+
 passwd
 exit
+
+umount /mnt/boot/efi
 umount /mnt/home
 umount /mnt
-#telinit 6
+telinit 6
