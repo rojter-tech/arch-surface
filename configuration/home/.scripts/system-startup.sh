c47ee2083c350060c30e783d47d4d2d06d8968c0
Daniel Reuter <dreuter@kth.se>
2020-03-07 18:22:08 +0100

    update

--- configuration/home/.scripts/system-startup.sh
+++ configuration/home/.scripts/system-startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-feh --bg-scale ~/.xmonad/bg/code-coding_3840x2400.jpg
 xrdb -load ~/.Xresources
-(~/.screenlayout/1920x1080/top/top_external.sh &)
+~/.screenlayout/1920x1080/top/top_external.sh
+feh --bg-scale ~/.xmonad/bg/code-coding_3840x2400.jpg
 (~/.scripts/connect_bt.sh &)
-(conky &)
 setxkbmap -option 'grp:alt_shift_toggle' -layout us,se -variant qwerty
 numlockx on
+conky
 echo "Sucess loading startup"
